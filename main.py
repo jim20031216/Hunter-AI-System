cfcd871777d4202996104c8970f0cbb3059a5d6a
jimchiu1216 <jim20031216@gmail.com>
2026-01-17 07:23:34 +0000

    Initial commit for deployment

--- main.py
+++ main.py
@@ -146,4 +146,4 @@ def run_analysis(mode):
 
 # ================= Main Entry Point for Web Server =================
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8081)
